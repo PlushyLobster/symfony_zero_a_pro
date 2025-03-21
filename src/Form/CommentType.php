b33dcd60c1a3b5eacce37d16fa16a3ca701c7732
Amélie <amelie.benoist@icloud.com>
2025-03-21 17:53:39 +0100

    Gérer les évènements de formulaire

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -5,13 +5,22 @@ namespace App\Form;
 use App\Entity\Article;
 use App\Entity\Comment;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class CommentType extends AbstractType
 {
+    private $user;
+
+    public function __construct(Security $security)
+    {
+        $this->user = $security->getUser();
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -26,6 +35,13 @@ class CommentType extends AbstractType
                 
             ])
         ;
+
+        $builder->addEventListener(FormEvents::POST_SET_DATA, function(FormEvent $event){
+            $form = $event->getForm();
+            if($this->user){
+                $form->get('author')->setData($this->user->getUserIdentifier());
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
